a7727683aca7878a65d4699ea094076d17d74b7e
Ramisk2000 <123549194+Ramisk2000@users.noreply.github.com>
2023-04-30 22:53:33 +0100

    Add files via upload

---
+++ bourse.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "PersistancePrixJournaliers.h"
+#include "date.ccp"
+
+
+using namespace std;
+
+
+class BourseVector {
+public:
+    BourseVector(vector<PrixJournalier>& pj)
+    {
+        for (PrixJournalier& p : pj)
+        {
+            historique.push_back(p);
+        }
+    }
+
+
+private:
+   vector<PrixJournalier> historique;
+};
+
+int main()
+    ifstream fichier("prices_simple.csv");
+    vector<PrixJournalier> prixJourn;
+    string ligne;
+    while (getline(fichier, ligne)) {
+        istringstream iss(ligne);
+        string dates, prixs;
+        iss >> dates >> prixs;
+        Date date = Date::parse_date(dates);
+        double prix = stod(prixs);
+        PrixJournalier pj(date, prix);
+        prixJourn.push_back(pj);
+    }
