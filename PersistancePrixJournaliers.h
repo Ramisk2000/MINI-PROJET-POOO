c9d481f9c95f002f2a29783a42ad019171b78420
Sakly Rami <123549194+Ramisk2000@users.noreply.github.com>
2023-05-04 02:58:26 +0100

    Update PersistancePrixJournaliers.h

--- PersistancePrixJournaliers.h
+++ PersistancePrixJournaliers.h
@@ -1,5 +1,5 @@
-#ifndef PERSISTANCEPRIXJOURNALIERS_H_INCLUDED
 #define PERSISTANCEPRIXJOURNALIERS_H_INCLUDED
+#include<iostream>
 #include<vector>
 #include<fstream>
 #include "prixjournaliers.h"
@@ -8,14 +8,17 @@ using namespace std;
 class PersistancePrixJournaliers
 {
     public:
-            static vector<PrixJournalier> lirePrixJournaliersDUnFichier(string chemin){
+            static vector<PrixJournalier> lirePrixJournaliersDUnFichier(string chemin)
+            {
             vector<PrixJournalier> historique;
             ifstream f(chemin);
             int nbLignes= 0;
             string entete;
-            if(f.is_open()){
+            if(f.is_open())
+            {
                 f>>entete;
-                while(!f.eof()){
+                while(!f.eof())
+                {
                     PrixJournalier pj;
                     f>>(pj);
                     historique.push_back(pj);
