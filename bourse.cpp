7a56b46dca450812a14a486903b06d32ca31afd6
Sakly Rami <123549194+Ramisk2000@users.noreply.github.com>
2023-05-04 02:59:48 +0100

    Update bourse.cpp

--- bourse.cpp
+++ bourse.cpp
@@ -1,39 +1,59 @@
+#ifndef BOURSEVECTOR_H_INCLUDED
+#define BOURSEVECTOR_H_INCLUDED
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 #include "PersistancePrixJournaliers.h"
 #include "date.h"
+#include "prixJournaliers"
 
 
 using namespace std;
 
 
-class BourseVector {
+class BourseVector
+{
+private:
+     vector<PrixJournalier> prixJournaliers;
 public:
-    BourseVector(vector<PrixJournalier>& pj)
-    {
-        for (PrixJournalier& p : pj)
+    BourseVector(const  string& fichier) {
+         ifstream f(fichier);
+         string nom;
+        int jj, mm , aaaa;
+        double prix;
+        while (f >> nom >> jj >> mm >> aaaa >> prix)
         {
-            historique.push_back(p);
+            PrixJournalier pj(nom, Date(j, m, a), prix);
+            prixJournaliers.push_back(pj);
         }
+         sort(prixJournaliers.begin(), prixJournaliers.end(), [](const PrixJournalier& pj1, const PrixJournalier& pj2) {
+            return pj1.getDate() < pj2.getDate();
+        });
+    }
+
+     vector< string> getActionsDisponiblesParDate(const Date& d) const {
+         vector< string> actions;
+        for (const auto& pj : prixJournaliers) {
+            if (pj.getDate() == d) {
+                actions.push_back(pj.getNom());
+            }
+        }
+        return actions;
+    }
+
+     vector<PrixJournalier> getPrixJournaliersParDate(const Date& d) const {
+         vector<PrixJournalier> prixJournaux;
+        for (const auto& pj : prixJournaliers) {
+            if (pj.getDate() == d) {
+                prixJournaux.push_back(pj);
+            }
+        }
+        return prixJournaux;
     }
 
 
-private:
-   vector<PrixJournalier> historique;
 };
 
-int main()
-    ifstream fichier("prices_simple.csv");
-    vector<PrixJournalier> prixJourn;
-    string ligne;
-    while (getline(fichier, ligne)) {
-        istringstream iss(ligne);
-        string dates, prixs;
-        iss >> dates >> prixs;
-        Date date = Date::parse_date(dates);
-        double prix = stod(prixs);
-        PrixJournalier pj(date, prix);
-        prixJourn.push_back(pj);
-    }
+
+#endif // BOURSEVECTOR_H_INCLUDED
